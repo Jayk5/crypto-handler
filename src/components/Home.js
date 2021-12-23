632950fdf62cf37a9b91e3f7b7671300c27e1b1a
Jayk5 <jayantkumar20@cse.iiitp.ac.in>
2021-12-24 01:45:19 +0530

    Fixed bug in Item display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import { useRef, useState, useEffect } from 'react'
-import Typography from '@mui/material/Typography';
-import Wrap from './Wrap';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import TextField from '@mui/material/TextField';
-import axios from 'axios';
-import List from './List';
-
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Wrap from "./Wrap";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import List from "./List";
 
 export default function Home() {
-
-    const searchRef = useRef('');
-    const [list, setList] = useState([])
-    const [search, setSearch] = useState('')
+    const searchRef = useRef("");
+    const [list, setList] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=gecko_desc&per_page=250&page=1&sparkline=false')
-            .then(res => {
-                setList(res.data)
+        axios
+            .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=volume_desc&per_page=250&page=1&sparkline=false")
+            .then((res) => {
+                setList(res.data);
             })
-            .catch(error => console.log(error))
-    }, [])
+            .catch((error) => console.log(error));
+    }, []);
 
-    const handleSearch = e => {
-        setSearch(e.target.value)
-    }
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
 
-    const filter = list.filter(l => l.name.toLowerCase().includes(search.toLowerCase()) || l.symbol.toLowerCase().includes(search.toLowerCase()))
+    const filter = list.filter(
+        (l) => l.name.toLowerCase().includes(search.toLowerCase()) || l.symbol.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <Wrap>
-            <Card alignItems='center' display='block' sx={{ maxWidth: 1000, mb: 3, pb: 1, borderBottom: 20, borderColor: 'primary.main' }}>
+            <Card alignItems="center" display="block" sx={{ maxWidth: 1000, mb: 3, pb: 1, borderBottom: 20, borderColor: "primary.main" }}>
                 <CardMedia
                     component="img"
                     alt="green iguana"
@@ -52,9 +53,8 @@ export default function Home() {
                 <CardActions>
                     <TextField fullWidth label="Search" id="fullWidth" inputRef={searchRef} onChange={handleSearch} />
                 </CardActions>
-
             </Card>
             <List data={filter} />
-        </Wrap >
-    )
+        </Wrap>
+    );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,60 +1,57 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
-import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import * as React from "react";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import ButtonBase from "@mui/material/ButtonBase";
+import FavoriteTwoToneIcon from "@mui/icons-material/FavoriteTwoTone";
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
 
 const auth = getAuth();
 const db = getFirestore();
 
-const Img = styled('img')({
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+const Img = styled("img")({
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
 });
 
 export default function Item({ id, name, image, symbol, price, volume, marketcap, priceChange, setList }) {
-
-    const [bstate, setbstate] = useState(false)
+    const [bstate, setbstate] = useState(false);
 
     async function clickHandler() {
         setbstate(true);
-        const snap = await getDoc(doc(db, "userFavs", auth.currentUser.email))
+        const snap = await getDoc(doc(db, "userFavs", auth.currentUser.email));
         if (snap.exists()) {
-            console.log(snap.data())
-            let x = snap.data().Favstr
-            console.log(x)
+            console.log(snap.data());
+            let x = snap.data().Favstr;
+            console.log(x);
             if (x.includes(`${id}%2C`)) {
-                x = x.replace(`${id}%2C`, '')
-                console.log('x replaced is this - ', x)
+                x = x.replace(`${id}%2C`, "");
+                console.log("x replaced is this - ", x);
                 await updateDoc(doc(db, "userFavs", auth.currentUser.email), {
-                    Favstr: x
-                })
+                    Favstr: x,
+                });
                 setList((list) => {
-                    return list.filter((item) => item.id !== id)
-                })
-            }
-            else {
+                    return list.filter((item) => item.id !== id);
+                });
+            } else {
                 await updateDoc(doc(db, "userFavs", auth.currentUser.email), {
-                    Favstr: snap.data().Favstr + id + "%2C"
-                })
+                    Favstr: snap.data().Favstr + id + "%2C",
+                });
             }
-        }
-        else {
+        } else {
             await setDoc(doc(db, "userFavs", auth.currentUser.email), {
-                Favstr: id + "%2C"
-            })
+                Favstr: id + "%2C",
+            });
         }
     }
 
     return (
-        <Paper sx={{ p: 2, margin: 'auto', maxWidth: 1000, flexGrow: 1, border: 3, mb: 1, borderColor: 'primary.main' }}>
+        <Paper sx={{ p: 2, margin: "auto", maxWidth: 1000, flexGrow: 1, border: 3, mb: 1, borderColor: "primary.main" }}>
             <Grid container spacing={2}>
                 <Grid item>
                     <ButtonBase sx={{ width: 100, height: 100 }}>
@@ -82,20 +79,23 @@ export default function Item({ id, name, image, symbol, price, volume, marketcap
                         <Typography variant="subtitle1" component="div">
                             ₹ {price.toLocaleString()}
                         </Typography>
-                        {priceChange < 0
-                            ?
+                        {priceChange < 0 ? (
                             <Typography color="error.main" variant="subtitle1" component="div">
-                                {priceChange.toFixed(2)}%
+                                {priceChange}%
                             </Typography>
-                            :
+                        ) : (
                             <Typography color="success.main" variant="subtitle1" component="div">
-                                {priceChange.toFixed(2)}%
+                                {priceChange}%
                             </Typography>
-                        }
-                        {auth.currentUser ? <FavoriteTwoToneIcon onClick={clickHandler} style={{ color: bstate ? 'red' : 'black' }} /> : " "}
+                        )}
+                        {auth.currentUser ? (
+                            <FavoriteTwoToneIcon onClick={clickHandler} style={{ color: bstate ? "red" : "black" }} />
+                        ) : (
+                            " "
+                        )}
                     </Grid>
                 </Grid>
             </Grid>
         </Paper>
-    )
+    );
 }
